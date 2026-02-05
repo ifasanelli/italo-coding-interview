0393f72b817f1badaca1459b291875f169ddb356
Italo Miranda <italo.miranda@locaweb.tech>
2026-02-05 11:09:24 -0300

    WIP Adds pagination #index in tweet controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,13 @@
 class TweetsController < ApplicationController
   def index
-    tweets = Tweet.by_user(params[:user_id])
+    page = params[:page].present? ? params[:page] : 0
+    per_page = params[:per_page].present? ? params[:per_page] : 5
+    tweets = Tweet
+              .by_user(params[:user_id])
+              .offset(page)
+              .limit(per_page)
+              .order(:id)
+
     render json: tweets
   end
 
@@ -8,5 +15,7 @@ class TweetsController < ApplicationController
 
   def search_params
     params.permit(:user_id)
+    params.permit(:page)
+    params.permit(:per_page)
   end
 end
